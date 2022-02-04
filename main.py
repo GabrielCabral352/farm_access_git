42bb8f83c24ef771133422d53b466d325728d6db
GabrielCabral352 <gabrielcabral352@gmail.com>
2022-02-04 11:14:43 -0300

    a151185ds

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from random import randint
+from multiprocessing import Process
 import os
 
-for i in range(10):
+
+
+
+def sendCommit():
     with open("commiter.txt", "a") as file:
         file.write(str(randint(1,200000)))
         os.system("git add *")
         os.system(f'git commit -m "a{randint(1,200000)}ds"')
         os.system("git push origin main")
         file.close()
-
+        
+if __name__ == '__main__':
+    for i in range(10):
+        Process(target=sendCommit).start()
+   
